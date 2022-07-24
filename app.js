2f8f04542b605f8f1bfcb2a45fc988c04a644146
PreetPatil4055 <104415175+PreetPatil4055@users.noreply.github.com>
2022-07-24 12:23:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const crypto = require('crypto');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
+const {GridFsStorage} = require('multer-gridfs-storage');
 const Grid = require('gridfs-stream');
 const methodOverride = require('method-override');
 
